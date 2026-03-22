b95fcc09d5097f775f4c01e5fa0f4b0d895b2f50
alwinrag59-rgb <alwinrag59@gmail.com>
2026-03-22 11:41:54 +0530

    fix

--- porche-app/next.config.ts
+++ porche-app/next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/Porche",
+  assetPrefix: "/Porche/",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
